46a37e7b2695aa94250ee4c114fd66f31c42be9b
Peter P. Lupo <pplupo@users.noreply.github.com>
2021-03-14 20:01:09 -0400

    Add files via upload
    Adding the first batch of batch files. ;-)

---
+++ bin/7Zip.bat
@@ -0,0 +1,4 @@
+rem zip file using 7-zip.
+rem -mxN where N is the compresison level.
+rem -tzip where zip is the format (supress the parameter for 7z).
+"C:\Program Files\7-Zip\7z.exe" a -tzip -mx0 filename.zip pathToCompress
---
+++ bin/readPropertiesFromFile.bat
@@ -0,0 +1,2 @@
+rem read key=value from file
+for /f "delims== tokens=1,2" %%G in (config.txt) do set %%G=%%H
